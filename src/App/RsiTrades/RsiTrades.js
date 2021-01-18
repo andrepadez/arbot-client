c1971eeaa9c8a422c7e62bdacefd67cc4d50061f
= <=>
2021-01-18 16:21:31 -0500

    rsi

--- src/App/RsiTrades/RsiTrades.js
+++ src/App/RsiTrades/RsiTrades.js
@@ -2,14 +2,20 @@ import React, { useState, useEffect } from 'react'
 
 const RsiTrades = ({ socket }) => {
   const [rsi, setRsi] = useState(0)
+  const [price, setPrice] = useState(0)
 
   useEffect(() => socket.on('rsi', setRsi), [socket])
+  useEffect(() => socket.on('btcPrice', setPrice), [socket])
 
   return (
     <div className='tickers'>
       <div className='ticker'>
         <h5>BTC/USDT RSI</h5>
         <div>{rsi.toFixed(2)}%</div>
+      </div>
+      <div className='ticker'>
+        <h5>BTC/USDT Price</h5>
+        <div>${price.toFixed(2)}</div>
       </div>
     </div>
   )
